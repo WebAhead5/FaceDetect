4f3cae7b46712ed1a61ba68570e85b132414192a
khaled <thenonexist@hotmail.com>
2020-03-25 00:33:55 +0200

    weather and style update

--- src/js/detectFaces.js
+++ src/js/detectFaces.js
@@ -1,7 +1,36 @@
 
-const giphyKey;
-const url;
+function showWeather(){
+    var weatherMapKey = "2ef6a42fb0c1fbf124ac800524fde470";
+    var city= document.getElementById("searchTxt").value;
+    if(city=="")
+        city="haifa"; //change this to current location when textField is empty or onload
+    var url="http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherMapKey;
 
+    (function() {
+        fetch(url)
+        .then(function(data) {
+          return data.json();
+        })
+        .then(function(weatherObj) {
+          document.getElementById("info1").innerHTML = weatherObj.weather[0].description;
+          document.getElementById("info2").innerHTML = "Humidity: " + weatherObj.main.humidity + "%";
+          document.getElementById("info3").innerHTML = "Temp: " + (Math.round(weatherObj.main.temp * (9/5) - 459.67)).toString() + " F";
+          document.getElementById("info4").innerHTML = "Wind speed: " + weatherObj.wind.speed + " m/s";
+          document.getElementById("conditionIcon").src="http://openweathermap.org/img/wn/" + weatherObj.weather[0].icon + ".png";
+        })
+        .catch(function(error) {
+          // clean info spans
+          for(var i=1;i<=4;i++)
+              document.getElementById("info"+i).innerHTML="";
+          document.getElementById("info1").innerHTML = "city not found!";
+        })
+      })();
+    
+}
+
+//=================
+var giphyKey;
+var url;
 function takeAction(){
     if(document.getElementById("searchTxt").value == ""){
         alert("Enter emotion");
