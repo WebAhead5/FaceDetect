c5ba3270d8090beb82389030e1b3e818797c69e7
khaled <thenonexist@hotmail.com>
2020-03-25 10:18:05 +0200

    new style and api

--- src/js/detectFaces.js
+++ src/js/detectFaces.js
@@ -12,23 +12,35 @@ function showWeather(){
           return data.json();
         })
         .then(function(weatherObj) {
+            //city name
+         document.getElementById("info0").innerHTML = weatherObj.name;
+            // weather description
           document.getElementById("info1").innerHTML = weatherObj.weather[0].description;
           document.getElementById("info2").innerHTML = "Humidity: " + weatherObj.main.humidity + "%";
+            // tempreture converted to F
           document.getElementById("info3").innerHTML = "Temp: " + (Math.round(weatherObj.main.temp * (9/5) - 459.67)).toString() + " F";
+            //wind speed
           document.getElementById("info4").innerHTML = "Wind speed: " + weatherObj.wind.speed + " m/s";
-          document.getElementById("conditionIcon").src="http://openweathermap.org/img/wn/" + weatherObj.weather[0].icon + ".png";
+            //weather icon
+          var weatherIcon=weatherObj.weather[0].icon;
+          document.getElementById("conditionIcon").src="http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+            //check icon for day or night
+          if(weatherIcon.substring(weatherIcon.length-1)=="d")
+              document.getElementById("conditionIcon").style.backgroundColor = "skyblue" ;
+          else
+              document.getElementById("conditionIcon").style.backgroundColor = "black";
         })
         .catch(function(error) {
           // clean info spans
-          for(var i=1;i<=4;i++)
+          for(var i=0;i<=4;i++)
               document.getElementById("info"+i).innerHTML="";
-          document.getElementById("info1").innerHTML = "city not found!";
+              document.getElementById("info1").innerHTML = "city not found!";
         })
       })();
     
 }
 
-//=================
+//Jake's giphy code
 var giphyKey;
 var url;
 function takeAction(){
