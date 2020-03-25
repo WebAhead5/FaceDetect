8b02b265ca780b798db1e1b14423ea57207c64ae
Morad Abed <morad890@gmail.com>
2020-03-25 11:59:21 +0200

    the place pic

--- src/js/detectFaces.js
+++ src/js/detectFaces.js
@@ -1,15 +1,14 @@
-
-const giphyKey;
-const url;
+// const giphyKey;
+// const url;
 
 function takeAction(){
     if(document.getElementById("searchTxt").value == ""){
         alert("Enter emotion");
     }else{
     var emotion = document.getElementById("searchTxt").value;
-    giphyKey = "bF0nNKQwebanW5ikOpCFyikV40BIDBsk"; // Jake's giphy key#
+    
     url = "http://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=" +  giphyKey;
-    //console.log("url is: ", url)
+    console.losg("url is: ", url)
     (function() {
         fetch(url)
         .then(function(data) {
@@ -29,22 +28,29 @@ function takeAction(){
     }
 
 //the location var here gonna be used from the ip location data
-    const pixaBayKey = "15724529-195ef5be27b1fe3bf39c5be31&q=";
-    var pixaUrl = "https://pixabay.com/api/?key=" + pixaBayKey + location + "&image_type=photo&pretty=true" ;
+function locationPic() {
+  var location = "haifa";
+  var pixaKey = "15724529-195ef5be27b1fe3bf39c5be31&q=";
+  var pixaUrl = "https://pixabay.com/api/?key=" + pixaKey + location + "&image_type=photo&pretty=true" ;
+  console.log(pixaUrl);
+  // https://pixabay.com/api/?key=15724529-195ef5be27b1fe3bf39c5be31&q=haifa&image_type=photo&pretty=true
+  var locPic = document.querySelector(".location");
+  fetch(pixaUrl)
+  .then(function(response){
+      return response.json();
+  })
+  .then(function(pics){
+      var picLink = pics.hits[1].largeImageURL;
+      locPic.src = picLink;
+  })
+  .catch(function(error){
+      console.log(error);
+      locPic.src = "https://cdn.pixabay.com/photo/2015/03/01/21/44/bart-655318_960_720.png";
+  })
+};
+locationPic();
+
+
     // var pixaUrl = "https://pixabay.com/api/?key=15724529-195ef5be27b1fe3bf39c5be31&q=haifa&image_type=photo&pretty=true"
 // console.log(pixaUrl)
-function locationImg(){
-    fetch(pixaUrl)
-    .then(function(data) {
-        return data.json();
-    })
-    .then(function locationEx(location) {
-        var randomId = parseInt(Math.random()*Object.keys(hits).length);  
-        var locationPic = document.getElementById('myImg');
-        var thePic = hits[randomId].previewURL;
-        locationPic.src = thePic;
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-} 
+
